806256e9b4f52fee620f3bd1983e195295bc3a7f
yForecasting <yves.sagaert@gmail.com>
2022-03-10 15:30:23 +0100

    Initial setup of the package

---
+++ R/salesforecasting_package.R
@@ -0,0 +1,9 @@
+#' salesforecasting: A package for sales forecasting in a business setting.
+#'
+#' This package provides functionalities for sales forecasting in a business 
+#' setting.
+#'
+#' @author  Yves R. Sagaert \email{yves.r.sagaert@gmail.com}
+#' @docType package
+#' @name salesforecasting
+"_PACKAGE"
